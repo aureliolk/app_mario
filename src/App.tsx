1608bd6ab52065ab185f659adfb481999e5c1a57
Aurélio Castro <aurelio.cos@outlook.com>
2022-08-29 08:14:05 -0300

    Adding function to Mario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import classnames from 'classnames'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jump, setJump] = useState(false)
+  console.log(jump)
+  
+  document.addEventListener("keydown",()=>{
+    setJump(true)
+    setTimeout(()=>{
+      setJump(false)
+    },1000)
+  })
+  
+
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='h-screen flex items-center justify-center'>
+      <div className='w-[80%] h-[500px] border rounded relative overflow-hidden'>
+          <img className='w-[80px] absolute bottom-0 animate-pipe' src="pipe.png" alt="Pipe Imagen" />
+          <img className={classnames('w-[150px] absolute bottom-0',{"animate-jump": jump})} src="mario.gif" alt="Mario Imagen" />
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,39 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pipe: 'pipe 5s linear infinite',
+        jump: 'jump 1s linear'
+      },
+      keyframes: {
+        jump:{
+          '0%':{
+            bottom: "0px"
+          },
+          '40%':{
+            bottom: "150px"
+          },
+          '50%':{
+            bottom: "150px"
+          },
+          '60%':{
+            bottom: "150px"
+          },
+          '100%':{
+            bottom: "0px"
+          }
+        },
+        pipe: {
+          "to": { 
+            right: "100%"
+           },
+          'from': { 
+            right: "0" 
+          },
+        }
+      }
+    },
   },
   plugins: [],
 }
